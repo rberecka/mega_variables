b4394eada5fd41d98037574dab9320961292fa75
Rachael Berecka <rachael.berecka@hashicorp.com>
2022-03-31 15:20:53 -0700

    mega vars

--- mega_variable_generator.rb
+++ mega_variable_generator.rb
@@ -4,10 +4,10 @@ require 'securerandom'
 (0..10000).each do |i|
   variable_name = SecureRandom.hex(7)
   variable_stanza = <<-HCL
-    variable "#{variable_name}" {
-      type = number
-      description = "#{i} Description of #{variable_name}"
-    }
+variable "#{variable_name}" {
+  type = number
+  description = "#{i} Description of #{variable_name}"
+}
   HCL
 
 print variable_stanza
